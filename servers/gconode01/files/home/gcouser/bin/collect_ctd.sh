e1138a1743d25c76e09b24a22b515b046f836d10
Rob Fuller <fullergalway@gmail.com>
2015-12-11 11:38:32 +0000

    wait 3 minutes after startup before running services

--- servers/gconode01/files/home/gcouser/bin/collect_ctd.sh
+++ servers/gconode01/files/home/gcouser/bin/collect_ctd.sh
@@ -8,9 +8,9 @@
 # Script is executed by supervisor,
 # see /etc/supervisor/conf.d/ctd.conf
 ################################################################## 
-while [ $(cat /proc/uptime | awk '{print $1}' | sed 's/\..*$//') -lt 120 ]
+while [ $(cat /proc/uptime | awk '{print $1}' | sed 's/\..*$//') -lt 180 ]
 do 
-   echo waiting for 2 minutes of uptime... >&2 
+   echo waiting for 3 minutes of uptime... >&2 
    sleep 10
 done
 
