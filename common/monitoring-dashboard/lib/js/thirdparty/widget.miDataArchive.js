20fe5aff19b63d54c1892059ecc4ec16fce0a20c
Rob Fuller <fullergalway@gmail.com>
2015-12-15 16:58:58 +0000

    allow a range of time when checking data archives

--- common/monitoring-dashboard/lib/js/thirdparty/widget.miDataArchive.js
+++ common/monitoring-dashboard/lib/js/thirdparty/widget.miDataArchive.js
@@ -19,39 +19,56 @@
 		
 		//array of our values: 0=Green, 2=Amber, 3=Red
 		var stateArray = ["green", "amber", "red"];
-        
-		function updateState() {         
-                        var dtype = currentSettings.device_type;
-                        var device = currentSettings.device;
-                        var ext = currentSettings.ext;
-                        var base = "http://spiddal.marine.ie/data/";
-                        var d = new Date(new Date().getTime() - (5*60*1000));
-                        var year = d.getUTCFullYear();
-                        var month = ("0"+(d.getUTCMonth()+1)).slice(-2);
-                        var day = ("0"+d.getUTCDate()).slice(-2);
-                        var hour = ("0"+d.getUTCHours()).slice(-2);
-                        var minute = ("0"+d.getUTCMinutes()).slice(-2);
-                        var filename = device+"_"+year+month+day+"_"+hour+minute+ext;
-                        var folder_url = base+dtype+"/"+device+"/"+year+"/"+month+"/"+day+"/";
-                        var url = folder_url+filename;
-                        stateObject.value = 1;
+                function getPossibleUrls(dtype,device,ext,quantity){
+                   var urls = [];
+                   for(var i=quantity; i>0; i--){
+                      var base = "http://spiddal.marine.ie/data/";
+                      var d = new Date(new Date().getTime() - (i*60*1000));
+                      var year = d.getUTCFullYear();
+                      var month = ("0"+(d.getUTCMonth()+1)).slice(-2);
+                      var day = ("0"+d.getUTCDate()).slice(-2);
+                      var hour = ("0"+d.getUTCHours()).slice(-2);
+                      var minute = ("0"+d.getUTCMinutes()).slice(-2);
+                      var filename = device+"_"+year+month+day+"_"+hour+minute+ext;
+                      var folder_url = base+dtype+"/"+device+"/"+year+"/"+month+"/"+day+"/";
+                      var url = folder_url+filename;
+                      urls.push(url);
+                   }
+                   return urls;
+                }
+                function verifyOneUrl(device,urls,prev_url){
+                   stateObject.value = 1;
+                   if(urls.length > 0){
+                      var url = urls.shift();
                       $.ajax({
                            url : url,
                            type : 'HEAD',
                           dataType: 'text/plain',
                            success : function(){
-                             stateObject["status"] = "<a target='"+device+"' href='"+folder_url+"'>OK</a>";
+                             stateObject["status"] = "<a target='"+device+"' href='"+url+"/..'>OK</a>";
                              stateObject["fails"] = 0;
                              stateObject.value=0;
+                            _updateState();
                           }
                       }).fail(function(){
-                           stateObject["status"] = "<a target='"+device+"' href='"+folder_url+"'>PROBLEM</a>";
-                           stateObject["fails"] += 1;
-                           stateObject.value= stateObject.fails>=5?2:1;
-                         }
-                       ).always(function(){
-                           _updateState();
-                         });  
+                             return verifyOneUrl(device,urls,url);
+                      });
+                   }else{
+                          stateObject["status"] = "<a target='"+device+"' href='"+prev_url+"/..'>PROBLEM</a>";
+                          stateObject["fails"] += 1;
+                          stateObject.value= stateObject.fails>=5?2:1;
+                         _updateState();
+                   }
+                }
+        
+		function updateState() {         
+                        var dtype = currentSettings.device_type;
+                        var device = currentSettings.device;
+                        var ext = currentSettings.ext;
+                        var quantity = parseInt(currentSettings.within_minutes);
+                        quantity = quantity? quantity: 5;
+                        var urls = getPossibleUrls(dtype,device,ext,quantity);
+                        verifyOneUrl(device,urls);
                 }
                 function _updateState() {
 		
@@ -121,6 +138,12 @@
             {
                 name: "ext",
                 display_name: "File Extension",
+                type: "text"
+            },
+            {
+                name: "within_minutes",
+                display_name: "Updated Within Minutes",
+                default_value: "5",
                 type: "text"
             }
         ],
