7524cd0e97321a63b8082dbd4bce26c33885a810
Rob Fuller <fullergalway@gmail.com>
2016-02-02 09:32:10 +0000

    allow till sometime later in the day for the warning to appear for the forecast dashboard

--- common/monitoring-dashboard/lib/js/thirdparty/widget.miErddapForecast.js
+++ common/monitoring-dashboard/lib/js/thirdparty/widget.miErddapForecast.js
@@ -23,7 +23,7 @@
                       var time = new Date(new Date().getTime() + (days*24*60*60*1000)).toISOString().substring(0,10);
                       return url.replace(/CHECKDATE/g,time);
                 }
-                function updateStateFromErddap(state,urls,prev_url){
+                function updateStateFromErddap(state,urls,prev_url,hours){
                    stateObject.value = 1;
                    if(urls.length > 0){
                       var url = urls.shift();
@@ -34,8 +34,11 @@
                            cache: true,
                            success : function(data){
                              if(data.table.rows.length == 0){
-                                return updateStateFromErddap(state+1,urls,url);
+                                return updateStateFromErddap(state+1,urls,url,hours);
                              }else{
+                                 if(state>0&&hours>0&&new Date().getHours()<hours){
+                                    state = state-1;
+                                 }
                                  var ok = state == 0?"OK":"LATE";
                                  stateObject["status"] = "<a target='_blank' href='"+url.replace("json","htmlTable")+"'>"+ok+"</a>";
                                  stateObject.value=state;
@@ -43,7 +46,7 @@
                              }
                           }
                       }).fail(function(){
-                             updateStateFromErddap(state+1,urls,url);
+                             updateStateFromErddap(state+1,urls,url,hours);
                       });
                    }else{
                           stateObject["status"] = "<a target='_blank' href='"+prev_url.replace("json","htmlTable")+"'>PROBLEM</a>";
@@ -54,11 +57,12 @@
         
 		function updateState() {         
                     var url = currentSettings.url;
+                    var hours = parseInt(currentSettings.hours);
                     var warn_days = parseInt(currentSettings.warn_days);
                     var error_days = parseInt(currentSettings.error_days);
                     var urls = [getDatedUrl(url,warn_days),
                                 getDatedUrl(url,error_days)];
-                    updateStateFromErddap(0,urls,urls[0]);
+                    updateStateFromErddap(0,urls,urls[0],hours);
                 }
                 function _updateState() {
 		
@@ -136,6 +140,20 @@
                 display_name: "ERROR if no data Y days into future",
                 default_value: "4",
                 type: "text"
+            },
+            {
+                name: "hours",
+                type: "option",
+                default_value: "0",
+                display_name: "Allowable delay",
+                options : [
+                   { name: "None", value: "0" },
+                   { name: "6am", value: "6" },
+                   { name: "9am", value: "9" },
+                   { name: "12 noon", value: "12" },
+                   { name: "3pm", value: "15" },
+                   { name: "6pm", value: "18" }
+                ]
             }
         ],
         newInstance: function (settings, newInstanceCallback) {
