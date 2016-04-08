5434fe833f5150dde2787fd29453764165680906
Rob Fuller <fullergalway@gmail.com>
2016-04-08 16:06:54 +0100

    updated heanet configuration

--- servers/spidvid/files/home/dmuser/bin/stream_udp_1236_to_heanet.sh
+++ servers/spidvid/files/home/dmuser/bin/stream_udp_1236_to_heanet.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+# old url was rtmp://193.1.189.55:1935/marine/marine
 mkdir -p /tmp/hls
 start=$(date +%s)
  /usr/local/bin/ffmpeg -i 'udp://226.0.0.1:1236' \
                        -vcodec libx264 \
                        -f flv \
-                       rtmp://193.1.189.55:1935/marine/marine
+                        "rtmp://transcode06.heanet.ie/35a780f500db4a28900a29119cd63da7?doConnect=TXGLOs0h"
