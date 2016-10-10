1c21ed8771b203f56cd19b1fc6fb52fe54ba65bf
Rob Fuller <fullergalway@gmail.com>
2016-10-10 16:27:01 +0100

    latest config from spidvid

--- servers/spidvid/files/home/dmuser/bin/record_stream.sh
+++ servers/spidvid/files/home/dmuser/bin/record_stream.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 seconds="$1"
 #/usr/local/bin/ffmpeg -t $seconds -f mpegts -i udp://226.0.0.1:1234 -vcodec copy -an -f segment  -segment_time 900 /home/dmuser/recordings/spiddal1-$(date +"%Y%m%d%H%M")-%03d.avi
-/usr/local/bin/ffmpeg -i udp://226.0.0.1:1234 -t "$seconds" -vcodec copy -an -f segment  -segment_time 900 /home/dmuser/recordings/spiddal1-$(date +"%Y%m%d%H%M")-%03d.avi
+#/usr/local/bin/ffmpeg -i udp://226.0.0.1:1234 -t "$seconds" -vcodec copy -an -f segment  -segment_time 900 /home/dmuser/recordings/spiddal1-$(date +"%Y%m%d%H%M")-%03d.avi
+TARGET_DIR=/nfs/video
+/usr/local/bin/ffmpeg -i udp://226.0.0.1:1234 -t "$seconds" -vcodec copy -an -f segment  -segment_time 900 $TARGET_DIR/spiddal1-$(date +"%Y%m%d%H%M")-%03d.avi
--- servers/spidvid/files/home/dmuser/bin/stream_video.sh
+++ servers/spidvid/files/home/dmuser/bin/stream_video.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 mkdir -p /tmp/hls
 start=$(date +%s)
+
+                       #-preset ultrafast \
 /usr/local/bin/ffmpeg -f decklink \
                       -i "DeckLink Mini Recorder@13" \
                      -vf "drawtext=expansion=normal:fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf:reload=1:textfile=/home/dmuser/timenow.txt: x=10: y=690: fontcolor=white: fontsize=20" \
                        -codec:v libx264 -crf 18 -preset ultrafast\
-                       -preset ultrafast \
-                       -r 25 \
                        -pix_fmt yuv420p \
+                       -r 25 \
                        -an \
                        -f mpegts \
                        udp://226.0.0.1:1234
