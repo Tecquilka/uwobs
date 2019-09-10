cd8e629fc4e2b75ac7d8af15e5fa2f8b28650705
Rob Fuller <fullergalway@gmail.com>
2019-09-10 14:40:57 +0100

    renamed ctd csv, and two fields in that file

--- common/apps/collectors/configure_ctd.py
+++ common/apps/collectors/configure_ctd.py
@@ -2,7 +2,7 @@
 
 from csv_select import select_one_from_csv
 
-csv = 'https://raw.githubusercontent.com/IrishMarineInstitute/uwobs/master/common/apps/test.csv'
+csv = 'https://raw.githubusercontent.com/IrishMarineInstitute/uwobs/master/common/apps/ctd_config.csv'
 query = '''
 SELECT * FROM csv
          WHERE reference='ctd0'
@@ -13,9 +13,9 @@ SELECT * FROM csv
 answer = select_one_from_csv(csv,query)
 print('# {}'.format(answer))
 # prints this:
-# {'index': 3, 'reference': 'ctd0', 'type': 'ctd', 'manufacturer': 'idronaut', 'id': 'I-OCEAN7-304-1214551', 'server': '172.16.255.5', 'port': 950, 'start_date': '2019-05-03T10:00:00Z', 'end_date': None}
+# {'index': 3, 'reference': 'ctd0', 'type': 'ctd', 'manufacturer': 'idronaut', 'id': 'I-OCEAN7-304-1214551', 'moxa_server': '172.16.255.5', 'moxa_port': 950, 'start_date': '2019-05-03T10:00:00Z', 'end_date': None}
 #
 print ('''DEVICE={}
 PORT={}
-SERVER={}'''.format(answer['id'],answer['port'],answer['server']));
+SERVER={}'''.format(answer['id'],answer['moxa_port'],answer['moxa_server']));
 
